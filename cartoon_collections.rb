dfd74ebd615c2cb0b49b29e4825e1a427e41fd92
Alethea Cary <aletheacary@gmail.com>
2019-05-19 21:56:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,6 @@ end
 def long_planeteer_calls(array)
   array.any? {|element| element.length > 4}
       true
-    else
-      false
-    end
-  end
 end
 
 def find_the_cheese# code an argument here

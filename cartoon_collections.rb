cf6d3b9ce9840139d9ba3d15ab171884f42c7ac3
Alethea Cary <aletheacary@gmail.com>
2019-05-19 21:47:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  array.collect do |element|
+    if element.length >
 end
 
 def find_the_cheese# code an argument here
